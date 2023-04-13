28aab559baf5d19dd6873516e2e2aa0150f789f4
wdthor <wdthor@proton.me>
2023-04-13 13:34:19 +0200

    US 1 - Afficher les films par popularité * Ajout style responsive

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container mt-4">
-    <div class="pt-4 mb-8 grid sm:grid-cols-2 md:grid-cols-4 grid-cols-1 gap-12">
+    <div class="pt-4 mb-8 grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 grid-cols-1 gap-12">
       <TheFilm v-for="(film, index) in listeFilm" :key="index" :film="film" />
     </div>
   </div>
